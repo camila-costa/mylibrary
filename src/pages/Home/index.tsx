b6de1be8bde9bf71c222b35f91165be4d7ffd083
Camila Costa <camila@dzaine.net>
2020-08-01 08:48:28 -0300

    Adiciona novos componentes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Menu from '../../components/Menu'
-// import dadosIniciais from '../../data/dados_iniciais.json';
-/* import BannerMain from '../../components/BannerMain';
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer'; */
+import Footer from '../../components/Footer';
 
 function Home() {
   return (
     <div style={{ background: "#141414" }}>
       <Menu />
 
-      {/* <BannerMain
+      <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
@@ -22,26 +22,31 @@ function Home() {
       />
 
       <Carousel
+        ignoreFirstVideo={false}
         category={dadosIniciais.categorias[1]}
       />
 
       <Carousel
+        ignoreFirstVideo={false}
         category={dadosIniciais.categorias[2]}
       />      
 
       <Carousel
+        ignoreFirstVideo={false}
         category={dadosIniciais.categorias[3]}
       />      
 
       <Carousel
+        ignoreFirstVideo={false}
         category={dadosIniciais.categorias[4]}
       />      
 
       <Carousel
+        ignoreFirstVideo={false}
         category={dadosIniciais.categorias[5]}
       />      
 
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
